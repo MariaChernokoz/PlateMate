13bb5c85c0ec01076e14c0421c2195ecdceffff7
MariaChernokoz <me@chernokoz.ru>
2025-04-18 02:06:58 +0300

    Initial Commit

---
+++ PlateMateeTests/PlateMateeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlateMateeTests.swift
+//  PlateMateeTests
+//
+//  Created by Chernokoz on 18.04.2025.
+//
+
+import Testing
+@testable import PlateMatee
+
+struct PlateMateeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
